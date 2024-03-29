81aeb3d9c5f26de9550edc1ebceb6a9d0457c75a
Danielokd <danielf.meloc@utadeo.edu.co>
2024-03-28 21:14:48 -0500

    adding new README and images

---
+++ PROJ_PYTHON_PZEM.ino
@@ -0,0 +1,45 @@
+#include <SoftwareSerial.h> // Arduino IDE <1.6.6
+#include <PZEM004T.h>
+
+
+PZEM004T pzem(2,3);  // (RX,TX) connect to TX,RX of PZEM
+IPAddress ip(192,168,1,1);
+
+void setup() {
+  Serial.begin(9600);
+  pzem.setAddress(ip);        
+}
+
+void loop() {
+  //voltaje RMS
+
+  float v = pzem.voltage(ip);
+  // Si hay error de comunicacion el voltaje = -1 por ello la condicion
+  if (v < 0.0) v = 0.0;
+  //Corriente RMS 
+
+  float i = pzem.current(ip);
+  //Potencia Promedio o activa 
+  float p = pzem.power(ip);
+  //potencia aparente
+  float s= v*i;
+  // reactive power
+  float p2= pow(p,2);
+  float s2= pow(s,2);
+  float s2p2=s2-p2;
+  bool complejo=false;
+  if(s2p2<0){
+    s2p2=abs(s2p2);
+    complejo=true ;
+    }
+  float q = sqrt(s2p2);
+  //factor de potencia
+  float fp=p/s;
+  if(i>0){
+    Serial.print(v);Serial.print(",");Serial.print(i);Serial.print(",");Serial.print(p);Serial.print(",");Serial.print(s);Serial.print(",");Serial.print(q);Serial.print(",");Serial.println(fp);  
+  }
+  else{
+    Serial.println("No hay una carga conectada");  
+  }
+  delay(172800); // Mil datos al cabo de 2 días 
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# electronica_proyecto_primer_corte
+# Proyecto Análisis de consumo PC Gamer en casa
+Este proyecto se basa en la obtención de los datos del sensor PZEM004T-V1, que nos permite ver la potencia activa que consume en este caso, el computador.
+Lo anterior se pudo realizar gracias a que se usó un programa llamado CoolTerm, que nos permite conectarnos al serial del Arduino y obtener los datos impresos, los cuales se imprimen separados por comas y en cierto orden:
+```
+ Serial.print(v);Serial.print(",");Serial.print(i);Serial.print(",");Serial.print(p);Serial.print(",");Serial.print(s);Serial.print(",");Serial.print(q);Serial.print(",");Serial.println(fp);  
+```
+Lo anterior se hizo de esta manera para poder visualizar correctamente el archivo csv y así mismo ser importado por medio de Python para una correcta visualización.
+## Montaje y Conexión 
+El montaje se relaciona con la imagen a continuación:
+[GitHub Logo](./images/montaje.jpeg)
+
